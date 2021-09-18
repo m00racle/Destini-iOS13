790932ce457ec47169be4cc3148d41c5c9ddebfe
m00racle <yanuar.prakosa@gmail.com>
2021-09-18 08:23:39 +0700

    add test target and gitignore log files

---
+++ Destini-iOS13Tests/Destini_iOS13Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Destini_iOS13Tests.swift
+//  Destini-iOS13Tests
+//
+//  Created by Yanuar Heru on 18/09/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import XCTest
+
+class Destini_iOS13Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
