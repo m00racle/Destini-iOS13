ce58d3dc8d972dbd25a0fe115367ec320a7a421d
m00racle <yanuar.prakosa@gmail.com>
2021-09-18 10:07:38 +0700

    connect IBAction to both buttons

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    
+    @IBAction func playerChoose(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
